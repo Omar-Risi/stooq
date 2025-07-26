2348e1916caa9349c5570f99178684807e6e3c85
reesi <=>
2025-07-26 13:45:13 +0400

    feat(add): product model relations added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Business;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'business_id',
+    ];
+
+    /**
+    * Get the business that owns this model.
+    *
+    * Defines an inverse one-to-many relationship to the Business model.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function business():BelongsTo {
+        return $this->belongsTo(Business::class);
+    }
 }
