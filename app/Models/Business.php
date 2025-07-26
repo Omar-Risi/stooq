fd7d465b77084f07353df3516cb53bdad13156c9
reesi <=>
2025-07-26 13:41:18 +0400

    feat(add): created business and product models

---
+++ app/Models/Business.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Product;
+
+class Business extends Model
+{
+
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_name',
+        'phone_number',
+        'social_media_handle',
+        'cr',
+        'profile_img',
+        'banner',
+    ];
+
+    /**
+    * Define a one-to-many relationship with the Product model.
+    *
+    * Returns all products associated with this model.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function products():HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ database/migrations/2025_07_26_090827_create_businesses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('owner_name');
+            $table->string('phone_number');
+            $table->string('social_media_handle');
+            $table->string('cr')->nullable(true);
+            $table->string('profile_img');
+            $table->string('banner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('businesses');
+    }
+};
