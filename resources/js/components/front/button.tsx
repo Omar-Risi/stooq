33d549966f3d6593b582443d56268b45bb335208
reesi <=>
2025-08-02 01:21:07 +0400

    feat(add): Added buttons to hero

---
+++ resources/js/components/front/button.tsx
@@ -0,0 +1,16 @@
+
+type ButtonProps = {
+    children?: any,
+    className?: string
+
+}
+
+export default function Button({ children, className }: ButtonProps) {
+
+
+    return (
+        <button className={`uppercase text-inherit bg-linear-to-r from-primary-dark to-primary px-4 py-2 rounded shadow-md cursor-pointer hover:bg-white hover:text-primary hover:bg-none transition-colors ${className}`}>
+            {children}
+        </button>
+    )
+}
--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,20 +1,25 @@
 import NavBar from "@/components/nav/navbar";
+import Button from "@/components/front/button";
 
 
 export default function Home() {
-
     return (
+
         <>
             <main className="relative flex flex-col min-h-[50vh]">
                 {/* BG made independent because of scale-x-125 */}
                 <div className="bg-linear-to-br from-light-magenta to-dark-magenta h-full scale-x-125 w-screen -z-5 rounded-b-full absolute top-0 left-0"></div>
 
-                <NavBar primaryColor="white"></NavBar>
+                <NavBar primaryColor={"white"}></NavBar>
 
                 <div className="text-white flex-1 flex justify-center items-center">
                     <div className="w-1/2">
                         <h1 className="text-4xl font-bold"> Welcome to <span className="bg-clip-text text-transparent bg-linear-to-r from-primary to-primary-dark"> STOOQ </span></h1>
                         <p> A Marketplace by students, for students</p>
+                        <div className="flex gap-4 mt-4">
+                            <Button>Become a vendor </Button>
+                            <Button className="bg-none border-4 text-primary border-primary hover:border-transparent"> Start shopping </Button>
+                        </div>
                     </div>
                 </div>
 
