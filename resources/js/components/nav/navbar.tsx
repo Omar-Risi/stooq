e4add71135b6a8317a83ca0a5622157f7fdf094a
reesi <=>
2025-08-02 14:53:05 +0400

    stlye(add): added padding to navbar links container

--- resources/js/components/nav/navbar.tsx
+++ resources/js/components/nav/navbar.tsx
@@ -14,7 +14,7 @@ export default function NavBar({ className }: NavBarProps) {
                 <h1 className="font-extrabold text-2xl"> STOOQ </h1>
             </Link>
 
-            <nav className="flex items-center gap-4">
+            <nav className="flex items-center gap-4 px-4">
                 <a href="#about"> About us </a>
                 <a href="#pricing"> Pricing </a>
                 <button className="rounded  shadow-md p-2 bg-primary text-white cursor-pointer hover:bg-white hover:text-primary transition-colors "> AR </button>
