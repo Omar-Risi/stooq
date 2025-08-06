87b7396c895da1b80f3978aaae24f2a4821f77fc
reesi <=>
2025-08-06 15:34:46 +0400

    feat(add): added ar translations

---
+++ resources/lang/ar/general.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'stooq' => 'ستوق',
+    'nav' => [
+        'become_a_vendor' => 'كن بائعًا',
+        'start_shopping' => 'ابدأ التسوق',
+        'about' => 'من نحن',
+        'pricing' => 'الأسعار'
+    ]
+];
+
---
+++ resources/lang/ar/home.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to',
+    'sub_headline' => 'A marketplace built by students, for students'
+];
