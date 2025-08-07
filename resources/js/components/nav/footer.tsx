73e059ec9322f0e5d4a2c7f481b82b0b39eedbac
reesi <=>
2025-08-07 15:49:15 +0400

    chore(translations): added translations to footer

--- resources/js/components/nav/footer.tsx
+++ resources/js/components/nav/footer.tsx
@@ -1,6 +1,8 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 export default function Footer() {
+
+    const { translations } = usePage().props;
 
     function ListItem({ url, children }) {
         return (
@@ -18,16 +20,22 @@ export default function Footer() {
     return (
         <footer className="mt-8 p-2 lg:py-4 lg:px-8 bg-primary text-white flex flex-col lg:flex-row lg:justify-between gap-8">
             <div>
-                <h1> STOOQ </h1>
+                <h1> {translations.general.stooq}</h1>
                 <ul className="mt-4">
-                    <ListItem url={route('home')}> Home </ListItem>
-                    <ListItem url={`${route('home')}#about`}> About us </ListItem>
-                    <ListItem url={route('business.sign-up')}> Become a vendor </ListItem>
+                    <ListItem url={route('home')}>
+                        {translations.general.nav.home}
+                    </ListItem>
+                    <ListItem url={`${route('home')}#about`}>
+                        {translations.general.nav.about}
+                    </ListItem>
+                    <ListItem url={route('business.sign-up')}>
+                        {translations.general.nav.become_a_vendor}
+                    </ListItem>
                 </ul>
             </div>
             <div className="flex justify-end items-end">
                 <p>
-                    Copyright @ STOOQ. All Rights Reserved.
+                    {translations.general.footer.rights}
                 </p>
             </div>
         </footer>
--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -3,9 +3,13 @@
 return [
     'stooq' => 'STOOQ',
     'nav' => [
+        'home'=>'Home',
         'become_a_vendor' => 'Become a vendor',
         'start_shopping' => 'Start shopping',
         'about' => 'About us',
         'pricing' => 'Pricing'
+    ],
+    'footer' => [
+        'rights'=>'Copyright @ STOOQ. All Rights Reserved.'
     ]
 ];
