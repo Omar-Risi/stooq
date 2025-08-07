828bb1d439a4cb95b89f50b6a7145ed830c885d2
reesi <=>
2025-08-07 16:55:45 +0400

    feat(add): created card boilerplate

--- resources/js/pages/business/sign-up.tsx
+++ resources/js/pages/business/sign-up.tsx
@@ -2,12 +2,31 @@ import NavBar from "@/components/nav/navbar";
 import Button from "@/components/front/button";
 import SubHeading from "@/components/front/sub-heading";
 import { Link, usePage } from "@inertiajs/react";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 
 export default function SignUp() {
 
     const { direction, translations } = usePage().props;
 
+    function InputContainer({ children }) {
+        return (
+            <div className="grid w-full items-center gap-3">
+                {children}
+            </div>
+        )
+    }
     return (
         <div dir={direction} className="h-full w-full">
             <main className="relative flex flex-col min-h-[50vh]">
@@ -23,7 +42,20 @@ export default function SignUp() {
 
 
             {/* Using min-h-[50vh] to make space dynamically instead of using margins */}
-            <section className="text-primary-dark min-h-[50vh] flex justify-center flex-col items-center text-center">
+            <section className="text-primary-dark min-h-[50vh] w-screen flex justify-center flex-col items-center text-center">
+
+                <Card className="w-1/2 text-start">
+                    <CardHeader>
+                        <CardTitle> Your information </CardTitle>
+                        <CardDescription> Provide information about yourself </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <InputContainer>
+                            <Label htmlFor="owner_name"> Name </Label>
+                            <Input type="text" placeholder="your name..." name="owner_name" />
+                        </InputContainer>
+                    </CardContent>
+                </Card>
             </section>
         </div>
     )
