7dc4ee86615acbcedb5a9229e1d0781963f85b59
reesi <=>
2025-08-09 15:03:42 +0400

    chore(add): added translations in the translation file

--- resources/lang/en/sign_up.php
+++ resources/lang/en/sign_up.php
@@ -1,4 +1,62 @@
 <?php
+
 return [
-    'headline' => 'Become a vendor at '
+    'headline' => 'Become a vendor at ',
+    'forms' => [
+        'owner' => [
+            'card' => [
+                'title' => 'Your Information',
+                'description' => 'Provide information about yourself',
+            ],
+            'name' => [
+                'label' => 'Name',
+                'placeholder' => 'John Doe',
+            ],
+            'age' => [
+                'label' => 'Age',
+                'placeholder' => 'Your age',
+            ],
+            'id_number' => [
+                'label' => 'ID number',
+                'placeholder' => 'Your id number',
+            ],
+            'education_level' => [
+                'label' => 'Education Level',
+                'options' => [
+                    'school' => 'School',
+                    'higher_education' => 'Higher Education',
+                ],
+            ],
+            'institute_name' => [
+                'label' => 'Educational institute name',
+                'placeholder' => 'GUtech',
+            ],
+            'phone_number' => [
+                'label' => 'Phone number',
+                'placeholder' => '+968 92345678',
+            ],
+            'email' => [
+                'label' => 'Email',
+                'placeholder' => 'youremail@business.com',
+            ],
+            'governorate' => [
+                'label' => 'Governorate in Oman',
+                'placeholder' => 'Select governorate',
+                'options' => [
+                    'Muscat' => 'Muscat',
+                    'Dhofar' => 'Dhofar',
+                    'Musandam' => 'Musandam',
+                    'Al Batinah North' => 'Al Batinah North',
+                    'Al Batinah South' => 'Al Batinah South',
+                    'Al Dakhiliyah' => 'Al Dakhiliyah',
+                    'Al Sharqiyah North' => 'Al Sharqiyah North',
+                    'Al Sharqiyah South' => 'Al Sharqiyah South',
+                    'Al Dhahirah' => 'Al Dhahirah',
+                    'Al Wusta' => 'Al Wusta',
+                    'Al Buraimi' => 'Al Buraimi',
+                ],
+            ],
+        ],
+    ],
 ];
+
