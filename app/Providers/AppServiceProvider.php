9a8118573c8ce68708e00b48d50eb915cfae4a72
Reesi <reesiomar@gmail.com>
2025-11-03 14:18:41 +0400

    fix: added https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Lang;
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // pass locale translations using props
         Inertia::share([
             'locale' => fn() => App::getLocale(),
